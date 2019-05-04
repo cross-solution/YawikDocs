27b108e0fd63a10d283907f828bf2b5a325ab237
Carsten Bleek <bleek@cross-solution.de>
2019-05-04 18:28:15 +0200

    set version to 0.33

--- source/conf.py
+++ source/conf.py
@@ -68,7 +68,7 @@ copyright = u'2019, CROSS Solution'
 # built documents.
 #
 # The short X.Y version.
-version = '0.33.11'
+version = '0.33'
 # The full version, including alpha/beta/rc tags.
 release = '0.33.11'
 
